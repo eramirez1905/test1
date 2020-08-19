df6122dababd3e00fb6c125c9a550a180d16bf62
diego <diego.garcia@pedidosya.com>
2020-08-19 17:27:23 -0300

    test dag

--- airflow/dags/example-dag.py
+++ airflow/dags/example-dag.py
@@ -1,38 +1,50 @@
 from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
-from airflow.operators.dummy_operator import DummyOperator
-
+from airflow import configuration as conf
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime.utcnow(),
-    'email': ['airflow@example.com'],
+    'start_date': datetime(2019, 1, 1),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
-    'retry_delay': timedelta(minutes=5)
+    'retry_delay': timedelta(minutes=5),
 }
 
+namespace = conf.get('kubernetes', 'NAMESPACE')
 
+# This will detect the default namespace locally and read the
+# environment namespace when deployed to Astronomer.
+if namespace =='default':
+    config_file = '/usr/local/airflow/include/.kube/config'
+    in_cluster=False
+else:
+    in_cluster=True
+    config_file=None
 
-passing = KubernetesPodOperator(namespace='airflow',
-                          image="Python:3.6",
-                          cmds=["Python","-c"],
-                          arguments=["print('hello world')"],
-                          labels={"foo": "bar"},
-                          name="passing-test",
-                          task_id="passing-task",
-                          get_logs=True,
-                          )
+dag = DAG('example_kubernetes_pod',
+          schedule_interval='@once',
+          default_args=default_args)
 
-failing = KubernetesPodOperator(namespace='airflow',
-                          image="ubuntu:1604",
-                          cmds=["Python","-c"],
-                          arguments=["print('hello world')"],
-                          labels={"foo": "bar"},
-                          name="fail",
-                          task_id="failing-task",
-                          get_logs=True,
-                          )
+# This is where we define our desired resources.
+compute_resources = \
+  {'request_cpu': '800m',
+  'request_memory': '3Gi',
+  'limit_cpu': '800m',
+  'limit_memory': '3Gi'}
+
+with dag:
+    k = KubernetesPodOperator(
+        namespace=namespace,
+        image="hello-world",
+        labels={"foo": "bar"},
+        name="airflow-test-pod",
+        task_id="task-one",
+        in_cluster=in_cluster # if set to true, will look in the cluster, if false, looks for file
+        cluster_context='docker-for-desktop', # is ignored when in_cluster is set to True
+        config_file=config_file,
+        resources=compute_resources,
+        is_delete_operator_pod=True,
+        get_logs=True)
