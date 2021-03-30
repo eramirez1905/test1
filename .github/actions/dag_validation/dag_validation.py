ae65c1533a56a76cdcd8295214271df064a5b6d0
diego <diego.garcia@pedidosya.com>
2021-03-30 15:33:55 -0300

    test 17

--- .github/actions/dag_validation/dag_validation.py
+++ .github/actions/dag_validation/dag_validation.py
@@ -4,12 +4,31 @@ import unittest
 from airflow.models import DagBag
 
 
+# class TestDagIntegrity(unittest.TestCase):
+#     LOAD_SECOND_THRESHOLD = 2
+#     def setUp(self):
+#         DAGS_DIR = os.getenv('AIRFLOW_HOME')
+#         logging.info("DAGs dir : {}".format(DAGS_DIR))
+#         self.dagbag = DagBag(dag_folder = DAGS_DIR, include_examples = False)
+#     def test_import_dags(self):
+#         self.assertFalse(
+#             len(self.dagbag.import_errors),
+#             'DAG import failures. Errors: {}'.format(
+#                 self.dagbag.import_errors
+#             )
+#         )
+
+
+# suite = unittest.TestLoader().loadTestsFromTestCase(TestDagIntegrity)
+# unittest.TextTestRunner(verbosity=2).run(suite)
+
 class TestDagIntegrity(unittest.TestCase):
+
     LOAD_SECOND_THRESHOLD = 2
+
     def setUp(self):
-        DAGS_DIR = os.getenv('AIRFLOW_HOME')
-        logging.info("DAGs dir : {}".format(DAGS_DIR))
-        self.dagbag = DagBag(dag_folder = DAGS_DIR, include_examples = False)
+        self.dagbag = DagBag(dag_folder="dags", include_examples=False)
+
     def test_import_dags(self):
         self.assertFalse(
             len(self.dagbag.import_errors),
@@ -17,7 +36,6 @@ class TestDagIntegrity(unittest.TestCase):
                 self.dagbag.import_errors
             )
         )
-
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestDagIntegrity)
 unittest.TextTestRunner(verbosity=2).run(suite)
