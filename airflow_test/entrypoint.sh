07567ada915b7e1ee10fae95446c533cba1b3ea4
diego <diego.garcia@pedidosya.com>
2021-04-13 15:58:21 -0300

    test airflow

---
+++ airflow_test/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "Start Testing"
+echo "Req path : $1"
+echo "DAGs dir : $2"
+echo "Var path : $3"
+
+export AIRFLOW_HOME="/github/workspace/$2"
+export PYTHONPATH="${PYTHONPATH}:${AIRFLOW_HOME}"
+
+echo "Airflow Home : $AIRFLOW_HOME"
+echo "Python : $PYTHONPATH"
+
+pip install -r $1
+
+airflow initdb > /dev/null
+
+airflow variables --import $3
+
+pytest /dag_validation.py
